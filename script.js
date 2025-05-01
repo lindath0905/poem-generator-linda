7a5e462f03d03c7c3d77f843ff3a52d077545eef
lindath0905 <linda.thurner@outlook.at>
2025-05-01 17:25:58 +0200

    integrated api and axios

--- script.js
+++ script.js
@@ -1,15 +1,26 @@
-function generatePoem(event) {
-    event.preventDefault();
-
+function displayPoem(response) {
     new Typewriter("#poem", {
-        strings: "La tombe dit à la rose",
+        strings: response.data.answer,
         autoStart: true,
         delay: 1,
         cursor: "",
       });
 }
+
+function generatePoem(event) {
+    event.preventDefault();  
+
+let instructionsInput = document.querySelector("#user-instructions")
+let prompt = `Generate a poem about ${instructionsInput.value}.`;
+let context = "You are a great poet and love to write short but beautiful poems. Please make a 4 short paragraph poem."
+let apiKey = "df3t9495b3b3410e50b1aoab7d1cc149";
+let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
 let poemElement = document.querySelector("#poem")
 poemElement.innerHTML = ".."
 
-let poemFormElement = document.querySelector("#poem-generator-form");
+axios.get(apiUrl).then(displayPoem);
+}
+
+let poemFormElement = document.querySelector(".poem-generator-form");
 poemFormElement.addEventListener("submit", generatePoem);
